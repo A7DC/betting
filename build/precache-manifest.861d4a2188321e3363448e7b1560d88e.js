65229c9a12587b7f3554b3fc4d747f8a6376472f
Daniel Caine <audaci7y@gmail.com>
2019-03-11 20:16:48 -0700

    build, deploying to heroku

---
+++ build/precache-manifest.861d4a2188321e3363448e7b1560d88e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7f1147f12461d9246a5",
+    "url": "/static/js/main.ba3e171a.chunk.js"
+  },
+  {
+    "revision": "f2ab0c6eed1bf3e74b60",
+    "url": "/static/js/2.bad36961.chunk.js"
+  },
+  {
+    "revision": "b7f1147f12461d9246a5",
+    "url": "/static/css/main.b6e42f41.chunk.css"
+  },
+  {
+    "revision": "691942386f2baa21e703f4633ab3950a",
+    "url": "/index.html"
+  }
+];
